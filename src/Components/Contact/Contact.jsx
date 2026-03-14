2ac9c1ca4273383e6be0b5a30c6e020dd0238acd
Ziad Abbas <tt933825@gmail.com>
2026-03-14 03:49:52 +0200

    testing - blur-- v4

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,5 +1,4 @@
 "use client";
-import useAnimate from "@/Hooks/useAnimate";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import React, { useRef, useState } from "react";
@@ -9,40 +8,11 @@ import Swal from "sweetalert2";
 gsap.registerPlugin(ScrollTrigger);
 
 const Contact = () => {
-  const formHeadingRef = useRef(null);
-  const formHeadingLineRef = useRef(null);
 
 
   const [status, setStatus] = useState("");
 
-  useAnimate(() => {
-    const path = document.querySelector("#pathToAnimate2");
-
-    if (!path) return;
-    const le = path.getTotalLength();
-
-
-    
-
-    gsap.set(path, {
-      strokeDasharray: le,
-      strokeDashoffset: le,
-      visibility: "visible",
-    });
-
-    gsap.to(path, {
-      strokeDashoffset: 0,
-      duration: 3,
-      ease: "power2.out",
-      scrollTrigger: {
-        trigger: "#pathToAnimate2",
-        start: "top 110%",
-        end: "bottom center",
-        scrub: 2,
-        scroller: "[data-scroll-container]",
-      },
-    });
-  });
+  
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -176,41 +146,12 @@ const Contact = () => {
               className="-z-20 absolute inset-0 -left-32 sm:-left-48 md:-left-52 top-10 sm:-top-20 object-contain w-[200px] sm:w-[300px] md:w-[400px]"
             />
 
-            <div className="">
-              <div className="absolute inset-0 top-26 right-120 z-99 mix-blend-exclusion pointer-events-none max-sm:inset-0 max-sm:right-10 max-sm:bottom-76">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="100%"
-                  height="100%"
-                  viewBox="0 0 1718 491"
-                  fill="none"
-                  preserveAspectRatio="xMidYMid meet"
-                  aria-hidden="true"
-                  role="img"
-                  className="w-[94%] max-sm:w-[120%]"
-                >
-                  <g>
-                    <path
-                      d="M-16.9136 489.299C-13.7043 477.36 -5.83596 467.019 1.39852 457.243C43.77 399.988 94.4031 349.142 146.141 300.466C235.078 216.791 328.218 136.876 425.213 62.6901C443.224 48.9141 468.997 30.0855 488.657 16.8407C493.638 13.4846 498.753 10.3267 503.898 7.22779C507.233 5.21897 513.309 -0.736034 514.741 2.88927C516.507 7.35998 511.638 12.0137 509.522 16.3273C498.984 37.8054 487.145 58.6916 474.575 79.0365C428.926 152.924 376.324 222.171 327.881 294.184C309.913 320.894 295.181 343.004 280.193 371.115C268.836 392.416 255.88 418.287 255.26 443.278C254.361 479.541 297.141 475.496 321.305 470.62C424.963 449.704 521.157 368.403 574.905 278.924C577.444 274.696 583.23 269.082 580.273 265.133C579.473 264.066 577.58 264.865 576.276 265.137C570.489 266.348 561.015 270.737 556.471 272.915C519.43 290.672 485.849 316.475 456.281 344.81C431.115 368.927 401.233 398.897 388.327 432.204C380.282 452.967 401.408 445.75 412.715 440.541C476.204 411.297 533.66 365.59 585.971 319.989C636.077 276.311 682.012 228.251 727.382 179.752C729.311 177.691 744.755 161.399 732.435 173.379C711.446 193.79 692.342 215.975 675.16 239.694C671.687 244.488 633.087 294.467 640.447 301.926C647.309 308.88 660.097 301.809 669.653 299.823C723.543 288.625 776.837 273.126 829.419 257.044C920.337 229.236 1009.59 196.465 1098.89 163.929C1099.35 163.76 1149.24 141.726 1150.06 150.066C1152.29 172.865 1150.33 197.066 1147.44 219.707C1145.34 236.216 1144.54 259.675 1167.46 258.417C1202.3 256.504 1238.33 234.155 1268.45 219.047C1339.63 183.338 1410.5 147.803 1485.42 120.354C1530.51 103.833 1581.51 84.119 1630.3 82.2506C1643.38 81.7496 1649.9 87.5531 1660.57 93.6864C1677.66 103.514 1696.98 102.138 1715.93 100.667"
-                      stroke="#ffffff"
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      id="pathToAnimate2"
-                      style={{
-                        strokeDashoffset: "0px",
-                        strokeDasharray: "3805.57",
-                      }}
-                    ></path>
-                  </g>
-                </svg>
-              </div>
-            </div>
+            
           </div>
 
           {/* Right Side - Form */}
           <div className="space-y-8 w-full max-w-2xl place-self-end max-sm:place-self-start mt-48 max-sm:mt-0 lg:ml-8 font-bold px-4 lg:px-12 max-sm:px-0">
             <div
-              ref={formHeadingRef}
               className="text-5xl pb-4 max-sm:pb-6 max-sm:text-[2.5rem] max-sm:leading-tight"
               style={{
                 fontFamily: '"Work Sans", sans-serif',
@@ -218,7 +159,7 @@ const Contact = () => {
               }}
             >
               <div style={{ overflow: "hidden" }}>
-                <h2 ref={formHeadingLineRef}>
+                <h2 >
                   LET'S COOK{" "}
                   <span className="text-4xl font-extralight max-sm:text-[2rem] max-sm:block">
                     TOGETHER
